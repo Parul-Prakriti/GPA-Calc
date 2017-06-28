da3b9261858316f5ca26211da07af0b44743d062
Parul Prakriti <parul.prakriti@gmail.com>
2017-06-28 10:40:58 +0530

    Updated files

--- app/src/main/java/com/example/parul/gpacalculator/MainActivity.java
+++ app/src/main/java/com/example/parul/gpacalculator/MainActivity.java
@@ -1,50 +1,98 @@
-package com.example.parul.gpacalculator;
+    package com.example.parul.gpacalculator;
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.EditText;
+    import android.os.Bundle;
+    import android.support.v7.app.AppCompatActivity;
+    import android.view.View;
+    import android.widget.Button;
+    import android.widget.EditText;
+    import android.widget.LinearLayout;
+    import android.widget.TextView;
+    import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+    import java.util.ArrayList;
+    import java.util.List;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    EditText NumberOfSubjectsTxt = (EditText)findViewById(R.id.txt_subjects);
-    int noOfSubject = Integer.parseInt(NumberOfSubjectsTxt.getText().toString());
-    int gpa =0;
+    public class MainActivity extends AppCompatActivity {
+        private LinearLayout mLayout;
+        private EditText mEditCreditText;
+        private EditText mEditGradeText;
+        private Button mButton;
+        private double perSubjectPoint=0;
+        private  int totalCredits=0;
+        List<EditText> allCredits = new ArrayList<>();
+        List<EditText> allGrades = new ArrayList<>();
 
-    public void pressOkay(int number){
-        for (int i=1; i<=number;i++){
-            EditText NumberOfCreditsTxt = (EditText)findViewById(R.id.txt_credits);
-            int noOfCerdits = Integer.parseInt(NumberOfCreditsTxt.getText().toString());
-            EditText GradeTxt = (EditText)findViewById(R.id.txt_grade);
-            String grade = GradeTxt.getText().toString();
-            gpa =gpa+calcGpa(noOfCerdits,grade);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+               super.onCreate(savedInstanceState);
+               setContentView(R.layout.activity_main);
+               mLayout = (LinearLayout) findViewById(R.id.linearLayout);
+               mEditCreditText = (EditText) findViewById(R.id.txt_credits);
+               mEditGradeText =  (EditText) findViewById(R.id.txt_grade);
+               mButton = (Button) findViewById(R.id.button);
+               mButton.setOnClickListener(onClick());
+               TextView textView = new TextView(this);
         }
-    }
+        private View.OnClickListener onClick() {
+            return new View.OnClickListener() {
 
-    public int calcGpa(int number,String grade){
-        int point;
-        if(grade.equalsIgnoreCase("S"))
-            point = 10;
-        else if (grade.equalsIgnoreCase("A"))
-            point =9;
-        else if (grade.equalsIgnoreCase("B"))
-            point = 8;
-        else if (grade.equalsIgnoreCase("C"))
-            point = 7;
-        else if (grade.equalsIgnoreCase("D"))
-            point = 6;
-        else if (grade.equalsIgnoreCase("E"))
-            point = 5;
-        else if (grade.equalsIgnoreCase("F"))
-            point = 4;
-        else point = 3;
+                   @Override
+                   public void onClick(View v) {
 
-        int gpa = number*point;
-        return gpa;
+                       mLayout.addView(createNewTextView(mEditCreditText.getText().toString(),mEditGradeText.getText().toString()));
+                       allCredits.add(mEditCreditText);
+                       allGrades.add(mEditGradeText);
+
+
+                        }
+                    };
+        }
+        private TextView createNewTextView(String text1,String text2) {
+               final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+               final TextView textView = new TextView(this);
+               textView.setLayoutParams(lparams);
+               textView.setText("Credit: "+text1+ "\nGrade: "+ text2);
+               return textView;
+        }
+
+
+        public double pointPerSubj(int number,String grade){
+                    int point;
+                    if(grade.equalsIgnoreCase("S"))
+                        point = 10;
+                    else if (grade.equalsIgnoreCase("A"))
+                        point =9;
+                    else if (grade.equalsIgnoreCase("B"))
+                        point = 8;
+                    else if (grade.equalsIgnoreCase("C"))
+                        point = 7;
+                    else if (grade.equalsIgnoreCase("D"))
+                        point = 6;
+                    else if (grade.equalsIgnoreCase("E"))
+                        point = 5;
+                    else point =0;
+
+                    double perSubjectPoint = (double)number*point;
+                    return perSubjectPoint;
+        }
+
+        public void calcGpa(View view){
+                    int size = allCredits.size();
+                    String[] stringsGrade = new String[size];
+                    Integer[] intCredits = new Integer[size];
+                    for(int i=0; i < size; i++){
+                        stringsGrade[i] = allGrades.get(i).getText().toString();
+                        intCredits[i] = Integer.parseInt(allCredits.get(i).getText().toString());
+                        totalCredits =totalCredits+intCredits[i];
+                        perSubjectPoint = perSubjectPoint+pointPerSubj(intCredits[i],stringsGrade[i]);
+                    }
+                    double gpa = perSubjectPoint/totalCredits;
+
+                    Toast.makeText(this,"Your GPA is: "+gpa, Toast.LENGTH_LONG).show();
+        }
+
+
+
+
     }
-}
 
